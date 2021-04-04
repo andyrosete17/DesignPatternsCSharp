c8184d9575c4b666dc7452cb55134e5229080cc9
andy rosete <andyrosete17@gmail.comm>
2021-04-04 17:19:27 +0200

    Singleton patterns

---
+++ 6-SingletonPattern/LoggingExample/Program.cs
@@ -0,0 +1,19 @@
+namespace LoggingExample
+{
+    using System;
+    using LoggingExample.Singleton;
+
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // get the logger instance
+            Logger logger = Logger.Instance;
+
+            // log three lines
+            logger.Log(string.Format("Logging started at {0}", DateTime.Now));
+            logger.Log("Hello World!");
+            logger.Log(string.Format("Logging ended at {0}", DateTime.Now));
+        }
+    }
+}
---
+++ 6-SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SingletonPattern
+{
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			// get the singleton instance
+			Singleton instance = Singleton.Instance;
+		}
+	}
+}
