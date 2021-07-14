1c9f44ee2195319f1afdd47d02bcb99bde3a542b
andy rosete <andyrosete17@gmail.comm>
2021-07-14 17:51:23 +0200

    update info

--- 3-StructuralPattern/7-ProxyPattern/ProxyPattern/Program.cs
+++ 3-StructuralPattern/7-ProxyPattern/ProxyPattern/Program.cs
@@ -1,21 +1,21 @@
-using System;
-
 namespace ProxyPattern
 {
-	/// <summary>
-	/// The main program class.
-	/// </summary>
-	class MainClass
-	{
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name="args">The command-line arguments.</param>
-		public static void Main (string[] args)
-		{
-			// execute the client
-			var client = new Client ();
-			client.Execute ();
-		}
-	}
+    using ProxyPattern_4_Client;
+
+    /// <summary>
+    /// The main program class.
+    /// </summary>
+    internal class MainClass
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            // execute the client
+            var client = new Client();
+            client.Execute();
+        }
+    }
 }
