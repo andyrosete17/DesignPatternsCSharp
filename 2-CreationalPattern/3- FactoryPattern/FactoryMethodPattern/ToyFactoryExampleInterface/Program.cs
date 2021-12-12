216fe10a73c059de7f2324b80be0cf5d480ca5e2
andyrosetenovafori <andy.rosete@perfectchannel.com>
2021-12-12 11:49:08 +0100

    update factory pattern code to have a switch

--- 2-CreationalPattern/3- FactoryPattern/FactoryMethodPattern/ToyFactoryExampleInterface/Program.cs
+++ 2-CreationalPattern/3- FactoryPattern/FactoryMethodPattern/ToyFactoryExampleInterface/Program.cs
@@ -2,23 +2,46 @@ using System;
 
 namespace ToyFactoryExample
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			ToyCreator creator = null;
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // create a firetruck
+            Console.WriteLine("Creating a firetruck toy:");
+            ToyCreator creator = new FiretruckCreator();
+            creator.MakeToy();
+            Console.WriteLine();
 
-			// create a firetruck
-			Console.WriteLine ("Creating a firetruck toy:");
-			creator = new FiretruckCreator();
-			creator.MakeToy();
-			Console.WriteLine ();
+            // create a superman
+            Console.WriteLine("Creating a superman toy:");
+            creator = new SupermanCreator();
+            creator.MakeToy();
+            Console.WriteLine();
 
-			// create a superman
-			Console.WriteLine ("Creating a superman toy:");
-			creator = new SupermanCreator();
-			creator.MakeToy();
-			Console.WriteLine ();
-		}
-	}
+            var name = "firetruck";
+            ToyCreationSelector(name);
+            name = "superman";
+            ToyCreationSelector(name);
+            name = "weird";
+            ToyCreationSelector(name);
+        }
+
+        private static void ToyCreationSelector(string name)
+        {
+            switch (name)
+            {
+                case "firetruck":
+                    new FiretruckCreator().MakeToy();
+                    break;
+
+                case "superman":
+                    new SupermanCreator().MakeToy();
+                    break;
+
+                default:
+                    new DuckCreator().MakeToy();
+                    break;
+            }
+        }
+    }
 }
